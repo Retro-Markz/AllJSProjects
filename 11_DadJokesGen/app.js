54f5fb28950148c493a4dd272633e3ee2fa46780
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-12 08:12:56 -0600

    dad jokes done

--- 11_DadJokesGen/app.js
+++ 11_DadJokesGen/app.js
@@ -1,17 +1,18 @@
 const btnEl = document.getElementById("btn");
 const API_URL = "https://api.api-ninjas.com/v1/dadjokes";
 const API_KEY = "bFIJYDV3fzTtCnBlrBKw7A==NiuWP3c0fii2Rsad";
-
+const jokeEl = document.getElementById("joke");
 const options = {
   method: "GET",
   headers: {
-    "x-Api-Key": MY_API,
+    "x-Api-Key": API_KEY,
   },
 };
 
 async function getJoke() {
   const response = await fetch(API_URL, options);
   const data = await response.json();
+  jokeEl.innerText = data[0].joke;
 }
 
 btnEl.addEventListener("click", getJoke);
